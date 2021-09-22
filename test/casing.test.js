a7e784f287e9b0b8ee70ac2702db618ab023d0ba
Alexander Forbes-Reed <git@alex.forbes.red>
2021-09-22 10:18:57 +0100

    Update tests

--- test/casing.test.js
+++ test/casing.test.js
@@ -1,62 +1,95 @@
 const test = require('ava');
 const { snek, desnek } = require('../lib/casing.js');
 
-test('transform object keys from camelCase to snake_case', t => {
+test('transform object keys from camelCase to snake_case (node)', t => {
 	const input = {
-        foo: "foo",
-        fooBar: "foo_bar",
-        fooBar100: "foo_bar_100",
-        foo100bars: "foo_100bars",
-        foo200Bars: "foo_200_bars",
-        FooBar: "FooBar",
-        Foo: "Foo",
-        nestedObject: {
-            fooBar: "foo_bar",
-        },
-    };
+		foo: "foo",
+		fooBar: "foo_bar",
+		fooBar100: "foo_bar_100",
+		foo100bars: "foo_100bars",
+		foo200Bars: "foo_200_bars",
+		FooBar: "FooBar",
+		Foo: "Foo",
+		nestedObject: {
+			fooBar: "foo_bar",
+		},
+	};
 
-    const expected = {
-        foo: "foo",
-        foo_bar: "foo_bar",
-        foo_bar_100: "foo_bar_100",
-        foo_100bars: "foo_100bars",
-        foo_200_bars: "foo_200_bars",
-        FooBar: "FooBar",
-        Foo: "Foo",
-        nested_object: {
-            foo_bar: "foo_bar",
-        },
-    };
+	const expected = {
+		foo: "foo",
+		foo_bar: "foo_bar",
+		foo_bar_100: "foo_bar_100",
+		foo_100bars: "foo_100bars",
+		foo_200_bars: "foo_200_bars",
+		FooBar: "FooBar",
+		Foo: "Foo",
+		nested_object: {
+			foo_bar: "foo_bar",
+		},
+	};
+	t.deepEqual(snek(input), expected)
+});
 
-    t.deepEqual(snek(input), expected)
+test('transform object keys from camelCase to snake_case (browser)', t => {
+	const input = {
+		foo: "foo",
+		fooBar: "foo_bar",
+		fooBar100: "foo_bar_100",
+		foo100bars: "foo_100bars",
+		foo200Bars: "foo_200_bars",
+		FooBar: "FooBar",
+		Foo: "Foo",
+		nestedObject: {
+			fooBar: "foo_bar",
+		},
+	};
+
+	const expected = {
+		foo: "foo",
+		foo_bar: "foo_bar",
+		foo_bar_100: "foo_bar_100",
+		foo_100bars: "foo_100bars",
+		foo_200_bars: "foo_200_bars",
+		FooBar: "FooBar",
+		Foo: "Foo",
+		nested_object: {
+			foo_bar: "foo_bar",
+		},
+	};
+
+	global.window = {};
+
+	t.deepEqual(snek(input), expected)
+
+	global.window = void 0;
 });
 
 test('transform object keys from snake_case to camelCase', t => {
-    const input = {
-        foo: "foo",
-        foo_bar: "foo_bar",
-        foo_bar_100: "foo_bar_100",
-        foo_100bars: "foo_100bars",
-        foo_200_bars: "foo_200_bars",
-        FooBar: "FooBar",
-        Foo: "Foo",
-        nested_object: {
-            foo_bar: "foo_bar",
-        },
-    };
+	const input = {
+		foo: "foo",
+		foo_bar: "foo_bar",
+		foo_bar_100: "foo_bar_100",
+		foo_100bars: "foo_100bars",
+		foo_200_bars: "foo_200_bars",
+		FooBar: "FooBar",
+		Foo: "Foo",
+		nested_object: {
+			foo_bar: "foo_bar",
+		},
+	};
 
-    const expected = {
-        foo: "foo",
-        fooBar: "foo_bar",
-        fooBar100: "foo_bar_100",
-        foo100bars: "foo_100bars",
-        foo200Bars: "foo_200_bars",
-        FooBar: "FooBar",
-        Foo: "Foo",
-        nestedObject: {
-            fooBar: "foo_bar",
-        },
-    };
+	const expected = {
+		foo: "foo",
+		fooBar: "foo_bar",
+		fooBar100: "foo_bar_100",
+		foo100bars: "foo_100bars",
+		foo200Bars: "foo_200_bars",
+		FooBar: "FooBar",
+		Foo: "Foo",
+		nestedObject: {
+			fooBar: "foo_bar",
+		},
+	};
 
-    t.deepEqual(desnek(input), expected)
+	t.deepEqual(desnek(input), expected)
 });
